f23f44efbde0a2cc6da4e294da09fa1f3a59c3ae
hanum <rntls123@naver.com>
2021-07-18 00:31:31 +0900

    07.18 00:31 Routers

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-NodeJS practice project.
+## NodeJS practice project 🕹
+
+# Global Router
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+# User Router
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+# Video Router
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
 import express from "express";
-// babel 이용해 최신문법 적용
+import morgan from "morgan";
+// babel 이용해 최신문법 적용, import 뒤는 이름 상관없음 (python as 와 유사)
 // const express = require("express");
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowed")
-    next();
-}
-
-const handleHome = (req, res) => {
-    return res.end();
-}
-
-app.use(logger)
-app.use(privateMiddleware)
-app.get("/", handleHome)
+app.use(logger);
 // use 는 global middleware(모든 route 에서 사용) 를 만들어줌. **get 보다 먼저 와야함!
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+// Router 설정
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
 // ES6 함수 생성법 (매개변수) => 함수 console.log("Server listening on port 4000 🚀")

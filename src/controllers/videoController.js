0546746fe13d26bc9ad917adb2818fcbc9bbeefc
hanum <rntls123@naver.com>
2021-07-27 22:56:23 +0900

    07.27 22:55 Concept of async & await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,59 +1,59 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 40,
-        id: 1
-    },
-    {
-        title: "Second Video",
-        rating: 3,
-        comments: 12,
-        createdAt: "1 minutes ago",
-        views: 30,
-        id: 2
-    },
-    {
-        title: "Third Video",
-        rating: 1,
-        comments: 1,
-        createdAt: "5 minutes ago",
-        views: 1,
-        id: 3
-    },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    return res.render("home", { "pageTitle": "HOME", videos });
+// export default 를 하면 한 모듈에서 하나의 변수를 export 하게 됨.
+// 위와 같은 방법으로 한 파일에서 여러 개를 export 할 수 있음
+
+/* callback 이용한 코드.아래는 promise 이용.
+    export const home = (req, res) => {
+    Video.find({}, (error, videos) => {
+        console.log("errors", error);
+        console.log("videos", videos);
+        return res.render("home", { "pageTitle": "HOME", videos });
+    }); 
+    };
+*/
+
+/* find: mongoose model의 메소드. {} 는 모든 video 를 찾겠다는 걸 의미한다. 
+    django 의 objects 와 유사.
+    아래 callback 과 await 차이 설명 읽기.
+*/
+
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({})
+        // callback 과 다른 async await 비동기 처리 js 의 promise.
+        return res.render("home", { pageTitle: "Home", videos });
+    } catch {
+        return res.render("ERROR!");
+    }
 };
+
+// await 를 find 앞에 적어줌으로써 find 에 callback 이 필요하지 않음을 알려줌. promise 방식.
+// 위의 callback 형식으로 find 를 할 경우 find 다음의 코드가 find 완료를 기다리지 않고 실행 된 후
+// find 가 완료되는 시점에 find 함수가 callback 되어 실행됨.
+// 그러나 await 의 경우 find 가 완료되어야 다음 코드가 실행. 즉 DB 를 기다려 줌.
+//  이를 비동기 처리라 함. js 문법으로 async function 내에서만 await 사용 가능.
+
 
 export const watch = (req, res) => {
     const { id } = req.params
     // ES6 문법. const id = req.params.id 와 동일 
-    const video = videos[id - 1]
-
     return res.render("watch", {
-        pageTitle: `Watching: ${video.title}`,
-        video: video
+        pageTitle: `Watching: #${id}`,
     });
 }
 
 export const getEdit = (req, res) => {
     const { id } = req.params
-    const video = videos[id - 1]
 
     return res.render("edit", {
-        pageTitle: `Edit: ${video.title}`,
-        video: video
+        pageTitle: "Editing Video",
     });
 }
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -61,17 +61,5 @@ export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 }
 export const postUpload = (req, res) => {
-    const newVideo = {
-        title: req.body.title,
-        rating: 0,
-        comments: 0,
-        createdAt: "now",
-        views: 0,
-        id: videos.length + 1
-    };
-    videos.push(newVideo);
     return res.redirect("/");
 }
-
-// export default 를 하면 한 모듈에서 하나의 변수를 export 하게 됨.
-// 위와 같은 방법으로 한 파일에서 여러 개를 export 할 수 있음
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
 // export default 가 아닐 때 import 하는 방법
-import { trending, search } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 

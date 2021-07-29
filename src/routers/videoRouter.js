7cd547304405765fd53308f23ef4e2f9f29a6c48
hanum <rntls123@naver.com>
2021-07-30 02:09:57 +0900

    07.30 02:01 Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 
@@ -7,7 +7,7 @@ const videoRouter = express.Router();
 // videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 videoRouter.get("/upload", getUpload);
 videoRouter.post("/upload", postUpload);
 export default videoRouter;

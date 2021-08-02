a3e125be83d112b1a1b55d30cbb2cf07da122dad
hanum <rntls123@naver.com>
2021-08-02 23:32:44 +0900

    08.02 23:32 Form Validation (Check password, existing username, email )

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,16 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "가입하기" });
 export const postJoin = async (req, res) => {
-    const { email, username, password, name, location } = req.body;
-    await User.create({ email, username, password, name, location })
+    const { email, username, password1, password2, name, location } = req.body;
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    // $or operator -> mongoose 기능. array 내의 조건들을 or 로 판단해 결과 반환
+    if (password1 !== password2) {
+        return res.render("join", { pageTitle: "가입하기", errorMessage: "비밀번호가 서로 일치하지 않습니다." })
+    }
+    if (exists) {
+        return res.render("join", { pageTitle: "가입하기", errorMessage: "이미 사용중인 이름/이메일 입니다." })
+    };
+    await User.create({ email, username, password, name, location });
     res.redirect("/login");
 }
 export const edit = (req, res) => res.send("Edit User");

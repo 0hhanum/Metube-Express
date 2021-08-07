86848e85f26c9f11861626724be9f48944f23792
hanum <rntls123@naver.com>
2021-08-07 19:12:11 +0900

    08.07 19:11 protect url using publicOnlyMiddleware, protectorMiddleware

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 // export default 가 아닐 때 import 하는 방법
 import { home, search } from "../controllers/videoController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;

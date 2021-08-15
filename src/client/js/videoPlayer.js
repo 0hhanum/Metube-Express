e48e0a075ea4019c8fd637d1eae38f3ea59f9c0d
hanum <rntls123@naver.com>
2021-08-15 22:18:13 +0900

    08.15 22:17 Current time of Video & loaded meta data

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const play = document.getElementById("play");
 const mute = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 const video = document.querySelector("video");
 
@@ -38,6 +39,23 @@ const handleVolumeChange = (event) => {
     video.volume = volumeValue;
 }
 
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
 play.addEventListener("click", handlePlay);
 mute.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+// metaData 가 모두 load 되었을 때 실행
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+if (video.readyState == 4) {
+    handleLoadedMetadata();
+}
+// eventListener 가 추가되기 전에 video 가 모두 로딩되어 실행이 안될 수 있음.Math
+// readyState == 4 는 video 가 충분히 로딩되었음을 뜻함.

b14cc95bf5adf6f889c57086ff84e1e87d881f19
hanum <rntls123@naver.com>
2021-08-15 21:44:09 +0900

    08.15 21:43 Volume, Mute controller

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,11 @@
 const play = document.getElementById("play");
 const mute = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 const video = document.querySelector("video");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlay = (event) => {
     if (video.paused) {
@@ -14,7 +17,27 @@ const handlePlay = (event) => {
 };
 
 const handleMute = (event) => {
-
+    if (video.muted) {
+        video.muted = false;
+        video.volume = volumeValue;
+        volumeRange.value = volumeValue;
+    } else {
+        video.muted = true;
+        volumeRange.value = 0;
+    }
+    mute.innerText = video.muted ? "Unmute" : "Mute";
 };
+
+const handleVolumeChange = (event) => {
+    const { target: { value } } = event;
+    if (video.muted) {
+        video.muted = false;
+        mute.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = volumeValue;
+}
+
 play.addEventListener("click", handlePlay);
 mute.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);

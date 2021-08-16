63b75f24a0d62b56d4022ce23552043e6b450fe5
hanum <rntls123@naver.com>
2021-08-17 00:10:20 +0900

    08.17 00:09 Recording! (ondataavailable, recorder start & stop)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,13 @@ const video = document.getElementById("preview");
 
 // FE 에서 async await 를 사용하려면 regeneratorRuntime 설치해야함.
 // client/main.js 에서 import 했음. =>  base.pug 에서 scripts 로 받아오고 있음.
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+
+let stream;
+// 함수 간 stream 을 공유하기 위해 비어있는 변수 만들기.
+let recorder;
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: true,
     })
@@ -12,5 +17,35 @@ const handleStart = async () => {
     video.play();
 }
 
+const handleStop = () => {
+    startBtn.innerText = "다운로드";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+    recorder.stop();
+}
 
+const handleStart = () => {
+    startBtn.innerText = "녹화 중지";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        const videoFile = URL.createObjectURL(event.data);
+        // 이 url 은 브라우저 메모리에만 존재. 실제로는 X
+        video.srcObject = null;
+        // srcObject 에서 실제 file 의 src 로 변경
+        video.src = videoFile;
+        video.play();
+        video.loop = true;
+    };
+    // recorder 의 start 메소드가 완료되었을 때 발생하는 event 
+    // 위 라인은 console.log(recorder.ondataavailable) 과 동일한듯.   
+    recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+}
+
+
+init();
 startBtn.addEventListener("click", handleStart);

75388d87ad6fd4753d105e04cbad62c1c7721c7d
hanum <rntls123@naver.com>
2021-08-16 23:28:45 +0900

    08.16 23:28 Video record preview (srcObject)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,16 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 // FE 에서 async await 를 사용하려면 regeneratorRuntime 설치해야함.
 // client/main.js 에서 import 했음. =>  base.pug 에서 scripts 로 받아오고 있음.
-
 const handleStart = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
+        audio: false,
         video: true,
     })
-    console.log(stream);
+    video.srcObject = stream;
+    video.play();
 }
+
+
 startBtn.addEventListener("click", handleStart);

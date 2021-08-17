61ac039f3a1c7b08b3d992b05d2b505b6b58d748
hanum <rntls123@naver.com>
2021-08-17 22:13:56 +0900

    08.17 22:12 Download recorded video Using HTML anchor download

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,5 @@
 const startBtn = document.getElementById("startBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 const video = document.getElementById("preview");
 
 // FE 에서 async await 를 사용하려면 regeneratorRuntime 설치해야함.
@@ -7,6 +8,7 @@ const video = document.getElementById("preview");
 let stream;
 // 함수 간 stream 을 공유하기 위해 비어있는 변수 만들기.
 let recorder;
+let videoFile;
 
 const init = async () => {
     stream = await navigator.mediaDevices.getUserMedia({
@@ -17,10 +19,20 @@ const init = async () => {
     video.play();
 }
 
+const handleDownload = () => {
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
+};
+
+
 const handleStop = () => {
-    startBtn.innerText = "다운로드";
+    startBtn.innerText = "다시 녹화";
     startBtn.removeEventListener("click", handleStop);
     startBtn.addEventListener("click", handleStart);
+    downloadBtn.className = "showing"
     recorder.stop();
 }
 
@@ -28,9 +40,10 @@ const handleStart = () => {
     startBtn.innerText = "녹화 중지";
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
+    init();
     recorder = new MediaRecorder(stream);
     recorder.ondataavailable = (event) => {
-        const videoFile = URL.createObjectURL(event.data);
+        videoFile = URL.createObjectURL(event.data);
         // 이 url 은 브라우저 메모리에만 존재. 실제로는 X
         video.srcObject = null;
         // srcObject 에서 실제 file 의 src 로 변경
@@ -49,3 +62,4 @@ const handleStart = () => {
 
 init();
 startBtn.addEventListener("click", handleStart);
+downloadBtn.addEventListener("click", handleDownload);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,5 +11,5 @@ videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 videoRouter.get("/:id([0-9a-f]{24})/delete", protectorMiddleware, deleteVideo);
 videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.single("video"), postUpload);
-videoRouter.get("/record", recorder);
+videoRouter.get("/record", protectorMiddleware, recorder);
 export default videoRouter;
